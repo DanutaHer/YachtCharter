ff4e153df8a120be3bd3a1d63ba6c986d8d5bc75
Marta Polcyn <marta.anna.polcyn@gmail.com>
2020-08-30 11:55:14 +0200

    Project structure for package service

--- src/main/java/entities/Model.java
+++ src/main/java/entities/Model.java
@@ -1,4 +1,70 @@
 package entities;
 
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+/**
+ * Represents a model of a yacht
+ * @author Marta Polcyn
+ */
+
+@Entity(name = "models")
 public class Model {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "model_id")
+    private Long modelId;
+
+    @Column (name = "model_desc", nullable = false)
+    private String modelDescription;
+
+    @Column (name = "price_per_day", nullable = false)
+    private BigDecimal pricePerDay;
+
+    @Column (name = "price_per_week", nullable = false)
+    private BigDecimal pricePerWeek;
+
+    @Column (name = "bunk_count", nullable = false)
+    private int bunkCount;
+
+    public Long getModelId() {
+        return modelId;
+    }
+
+    public void setModelId(Long modelId) {
+        this.modelId = modelId;
+    }
+
+    public String getModelDescription() {
+        return modelDescription;
+    }
+
+    public void setModelDescription(String modelDescription) {
+        this.modelDescription = modelDescription;
+    }
+
+    public BigDecimal getPricePerDay() {
+        return pricePerDay;
+    }
+
+    public void setPricePerDay(BigDecimal pricePerDay) {
+        this.pricePerDay = pricePerDay;
+    }
+
+    public BigDecimal getPricePerWeek() {
+        return pricePerWeek;
+    }
+
+    public void setPricePerWeek(BigDecimal pricePerWeek) {
+        this.pricePerWeek = pricePerWeek;
+    }
+
+    public int getBunkCount() {
+        return bunkCount;
+    }
+
+    public void setBunkCount(int bunkCount) {
+        this.bunkCount = bunkCount;
+    }
 }
--- src/main/java/entities/Yacht.java
+++ src/main/java/entities/Yacht.java
@@ -1,4 +1,37 @@
 package entities;
 
+import javax.persistence.*;
+
+/**
+ * Represents a yacht item
+ * @author Marta Polcyn
+ */
+
+@Entity(name = "yachts")
 public class Yacht {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "yacht_id")
+    private Long yachtId;
+
+    @Column (name = "model_id")
+    @ManyToOne
+    private Long modelId;
+
+    public Long getYachtId() {
+        return yachtId;
+    }
+
+    public void setYachtId(Long yachtId) {
+        this.yachtId = yachtId;
+    }
+
+    public Long getModelId() {
+        return modelId;
+    }
+
+    public void setModelId(Long modelId) {
+        this.modelId = modelId;
+    }
 }
