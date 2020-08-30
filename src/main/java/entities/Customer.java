510a9fd2e4100d06aa93d1f8f4ecf2ced3ff7fea
uzytkownik <uzytkownik@kursGIT.pl>
2020-08-30 12:43:12 +0200

    Mapped customer and rent

--- src/main/java/entities/Customer.java
+++ src/main/java/entities/Customer.java
@@ -1,10 +1,67 @@
 package entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 public class Customer {
 
+    @Id
+    @GeneratedValue
+    @Column(name = "customer_id")
+    private Long customerId;
 
+    @Column
+    private String name;
+
+    @Column
+    private String phone;
+
+    @Column
+    private String email;
+
+    @Column
+    private String address;
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
--- src/main/java/entities/Rent.java
+++ src/main/java/entities/Rent.java
@@ -1,4 +1,68 @@
 package entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
 public class Rent {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "rent_id")
+    private Long rentId;
+
+    @Column(name = "yacht_id")
+    private Long yachtId;
+
+    @Column(name = "customer_id")
+    private Long customerId;
+
+    @Column(name = "rented_from")
+    private LocalDate rentedFrom;
+
+    @Column(name = "rented_to")
+    private LocalDate rentedTo;
+
+    public Long getRentId() {
+        return rentId;
+    }
+
+    public void setRentId(Long rentId) {
+        this.rentId = rentId;
+    }
+
+    public Long getYachtId() {
+        return yachtId;
+    }
+
+    public void setYachtId(Long yachtId) {
+        this.yachtId = yachtId;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public LocalDate getRentedFrom() {
+        return rentedFrom;
+    }
+
+    public void setRentedFrom(LocalDate rentedFrom) {
+        this.rentedFrom = rentedFrom;
+    }
+
+    public LocalDate getRentedTo() {
+        return rentedTo;
+    }
+
+    public void setRentedTo(LocalDate rentedTo) {
+        this.rentedTo = rentedTo;
+    }
 }
